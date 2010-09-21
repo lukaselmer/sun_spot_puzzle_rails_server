5f4104c2f5e336bbad45c0e45dbfe856ccfc8f2e
Lukas Elmer <lukas.elmer@gmail.com>
2010-09-21 19:48:28 +0200

    Refactoring

--- app/models/puzzle_game.rb
+++ app/models/puzzle_game.rb
@@ -1,7 +1,7 @@
 class PuzzleGame < ActiveRecord::Base
   named_scope :latest, :order => 'created_at DESC'
-  named_scope :fastest, :limit => 3, :order => 'cycle_times ASC'
-  named_scope :slowest, :limit => 3, :order => 'cycle_times DESC'
+  named_scope :fastest, :limit => 3, :order => 'time_in_milliseconds ASC'
+  named_scope :slowest, :limit => 3, :order => 'time_in_milliseconds DESC'
   named_scope :fewest_swaps, :limit => 3, :order => 'swap_times ASC'
   named_scope :most_swaps, :limit => 3, :order => 'swap_times DESC'
 
